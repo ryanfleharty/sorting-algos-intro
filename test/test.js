2767f253aee6bf26909c4fc0906c7dd7ef685a4b
Ryan Fleharty <ryan.fleharty@gmail.com>
2018-10-08 09:21:43 -0600

    fixed test file

--- test/test.js
+++ test/test.js
@@ -18,12 +18,12 @@ describe('bubble sort', ()=>{
 describe('insertion sort', ()=>{
     it('should return a given array in order', ()=>{
         let unsorted = [4,6,2,3,7,1,5]
-        bubbleSort(unsorted).should.be.eql([1,2,3,4,5,6,7])
+        insertionSort(unsorted).should.be.eql([1,2,3,4,5,6,7])
         let unsortedTwo = [1,2,3,4,5,6,0]
-        bubbleSort(unsortedTwo).should.be.eql([0,1,2,3,4,5,6])
+        insertionSort(unsortedTwo).should.be.eql([0,1,2,3,4,5,6])
     })
     it('should not freak out about duplicates', ()=>{
         let unsorted = [4,6,6,2,3,7,1,5,3]
-        bubbleSort(unsorted).should.be.eql([1,2,3,3,4,5,6,6,7])
+        insertionSort(unsorted).should.be.eql([1,2,3,3,4,5,6,6,7])
     })
 })
